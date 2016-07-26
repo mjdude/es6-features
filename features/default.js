7694107198b3de6ee94e069e112155d01ea5f3d6
mjdude <mo@syncspot.net>
2016-07-26 11:49:15 -0700

    seting up babel and first feature

---
+++ features/default.js
@@ -0,0 +1,12 @@
+// function sayHello (name = 'World'){
+//   console.log('Hello ' + name + '!');
+// };
+//
+// sayHello();
+// sayHello('Mo');
+
+function greetUser (user ={ name: 'Mo'}){
+  console.log('Hello ' + user.name);
+};
+
+greetUser();
